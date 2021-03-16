3cd71bfcd7fda66dfa5bc02b48b3472fd1f72f71
Ruowen Tang <ruowen.tang@gmail.com>
2021-03-15 20:20:47 -0400

    Created urls_show.ejs to display the second route

--- express_server.js
+++ express_server.js
@@ -13,6 +13,20 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL;
+  const templateVars = {
+    shortURL: shortURL,
+    longURL: urlDatabase[shortURL]
+  };
+  res.render("urls_show", templateVars);
+});
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
